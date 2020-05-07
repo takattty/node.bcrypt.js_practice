864675d63bb85b98a22bcb94cc2319a334823788
takattty <r9r_celtics_t9t@icloud.com>
2020-05-07 22:26:32 +0900

    practice hashed

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-'use srtict';
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+//const myPlaintextPassword = 'hello';
 
+/**
+ * Promiseによる制御ありの非同期処理
+ * @param { String } ユーザーが入力した平文パスワードが渡される
+ * @return { String } ハッシュ化された値が返ってきます
+ * 理由： Promiseによる制御で呼び出した関数がしっかり処理し終えてから.thenの処理を始める様に実装している為
+ */
+module.exports.generateHash = (myPlaintextPassword) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+        resolve (hash);
+      });
+    });
+  });
+}
 
-// bcrypt.genSalt(saltRounds, (err, salt) => {
-//   console.log('これ引数ね　　' + salt);
+// generateSalt().then((salt) => {
+//   console.log(salt);
 // });
-
-bcrypt.genSalt(saltRounds, (err, salt) => {
-  return salt;
-});
-const salts = bcrypt.genSalt(saltRounds);
-console.log('This is salt  ' + salts);
-
-// bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
-
-// })
-
-module.exports = bcrypt;
--- index2.js
+++ index2.js
@@ -6,5 +6,25 @@ const saltRounds = 10;
 const salts = require('./index');
 const myPlaintextPassword = 'aaaaaa';
 
-console.log('this is generated solt  ' + salts.genSalt(saltRounds));
-//console.log('this is hashed password  ' + bcrypt.hash(myPlaintextPassword, salt));
+/**
+ * 制御なしの非同期処理
+ * @return { Object } Promise { <pending> } が出力
+ * 理由： 25行目の出力の時点ではまだgenSalt関数の処理が終わっていないつまり、saltが無い状態だから
+ */
+// bcrypt.genSalt(saltRounds, (err, salt) => {
+//   console.log(salt);//ここの処理を制御する前に出力しちゃうとpendingになる
+//   return salt;
+// });
+
+// const salt = bcrypt.genSalt(saltRounds);
+// console.log(salt);
+
+
+/**
+ * Promiseによる制御ありの非同期処理
+ * この呼び出しを行うのが、パスワードを用いた新規作成のタイミング
+ * @param { String } ここはユーザが入力した平文のパスワードを渡す。
+ */
+salts.generateHash(myPlaintextPassword).then((hash) => {
+  console.log(hash);
+});
